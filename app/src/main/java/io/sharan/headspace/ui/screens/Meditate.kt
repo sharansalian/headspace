d2d41c1d5d8e57938dc88eb7ae3b4cdea5942567
Sharan Salian <sharansalian.dev@gmail.com>
2022-06-22 17:38:15 +0530

    Add sample course section

--- app/src/main/java/io/sharan/headspace/ui/screens/Meditate.kt
+++ app/src/main/java/io/sharan/headspace/ui/screens/Meditate.kt
@@ -1,11 +1,19 @@
 package io.sharan.headspace.ui.screens
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import io.sharan.headspace.R
 
 @Composable
 fun MeditateScreen() {
@@ -13,6 +21,36 @@ fun MeditateScreen() {
         modifier = Modifier.fillMaxSize(),
         contentAlignment = Alignment.Center
     ) {
-        Text(text = "Meditate Screen")
+        ExploreMeditation()
+    }
+}
+
+
+@Composable
+fun ExploreMeditation(courses: List<String> = List(1000) { "$it" }) {
+    LazyColumn(modifier = Modifier.padding(4.dp)) {
+        items(items = courses) { name ->
+            Row(
+                modifier = Modifier.padding(12.dp)
+            ) {
+
+                Column(
+                    modifier = Modifier
+                        .weight(1F)
+                        .padding(12.dp)
+                ) {
+                    Text(text = "Hello")
+                    Text(
+                        text = name,
+                        style = MaterialTheme.typography.h4.copy(fontWeight = FontWeight.ExtraBold)
+                    )
+                }
+                Image(
+                    painter = painterResource(id = R.drawable.ic_course_1),
+                    contentDescription = "Course name"
+                )
+            }
+        }
+
     }
 }

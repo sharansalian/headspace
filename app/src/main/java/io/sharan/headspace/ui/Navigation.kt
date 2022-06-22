bac3376132d58b9b9c940ea63d3eff0f70f42fe7
Sharan Salian <sharansalian.dev@gmail.com>
2022-06-22 17:14:49 +0530

    Refactor Separate Screens

--- app/src/main/java/io/sharan/headspace/ui/Navigation.kt
+++ app/src/main/java/io/sharan/headspace/ui/Navigation.kt
@@ -1,14 +1,11 @@
 package io.sharan.headspace.ui
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import io.sharan.headspace.ui.screens.FocusScreen
+import io.sharan.headspace.ui.screens.MeditateScreen
 
 @Composable
 fun Navigation(navHostController: NavHostController) {
@@ -22,24 +19,4 @@ fun Navigation(navHostController: NavHostController) {
         }
     }
 
-}
-
-@Composable
-fun MeditateScreen() {
-    Box(
-        modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center
-    ) {
-        Text(text = "Meditate Screen")
-    }
-}
-
-@Composable
-fun FocusScreen() {
-    Box(
-        modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center
-    ) {
-        Text(text = "Focus Screen")
-    }
 }
